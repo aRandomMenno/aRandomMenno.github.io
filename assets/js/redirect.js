7933c33d98dc987927deada65a31c88e93a15ff1
Menno Van Zoelen <Menno.van.zoelen@gmail.com>
2024-04-10 18:25:41 +0200

    blog
    + blog
    + minor changes
    + minor cleanup

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,24 +1,19 @@
 
 function redirect(place) {
-  if (place == 'yt') {
-    window.open('https://youtube.com/@aRandomMenno', '_blank');
-  } else if (place == 'home') {
-    window.location.href = 'https://aRandomMenno.github.io';
-  } else if (place == 'projects') {
-    window.location.href = 'https://aRandomMenno.github.io/projects';
-  } else if (place == 'privacy') {
-    window.location.href = 'https://aRandomMenno.github.io/privacy';
-  } else if (place == 'Spijkenisse') {
-    window.open('https://en.wikipedia.org/wiki/Spijkenisse', '_blank');
-  } else if (place == 'Finland') {
-    window.open('https://en.wikipedia.org/wiki/Finland', '_blank');
-  } else if (place == 'github') {
-    window.open('https://github.com/arandomMenno/', '_blank');
-  } else if (place == 'discord') {
-    window.open('https://discord.gg/vRpeAcKruF', '_blank')
-  } else if (place == 'RiMusic') {
-    window.open('https://github.com/fast4x/RiMusic', '_blank')
-  } else if (place == 'ViMusic') {
-    window.open('https://github.com/vfsfitvnm/ViMusic', '_blank')
-  }
+  if (place == 'yt') { window.open('https://youtube.com/@aRandomMenno', '_blank') }
+  if (place == 'home') { window.location.href = 'https://aRandomMenno.github.io' }
+  if (place == 'projects') { window.location.href = 'https://aRandomMenno.github.io/projects' }
+  if (place == 'privacy') { window.location.href = 'https://aRandomMenno.github.io/privacy' }
+  if (place == 'blog') { window.location.href = 'https://aRandomMenno.github.io/blog';}
+  if (place == 'Spijkenisse') { window.open('https://en.wikipedia.org/wiki/Spijkenisse', '_blank') }
+  if (place == 'Finland') { window.open('https://en.wikipedia.org/wiki/Finland', '_blank') }
+  if (place == 'github') { window.open('https://github.com/arandomMenno/', '_blank') }
+  if (place == 'discord') { window.open('https://discord.gg/vRpeAcKruF', '_blank') }
+  if (place == 'RiMusic') { window.open('https://github.com/fast4x/RiMusic', '_blank') } 
+  if (place == 'ViMusic') { window.open('https://github.com/vfsfitvnm/ViMusic', '_blank') }
+  if (place == 'murderdrones') { window.open('https://www.youtube.com/playlist?list=PLHovnlOusNLiJz3sm0d5i2Evwa2LDLdrg', '_blank') }
+}
+
+function april2024(place) {
+  if (place == 10) { window.open('https://aRandomMenno.github.io/blog/2024/april/10') }
 }
