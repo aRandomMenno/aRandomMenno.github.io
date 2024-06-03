9a82e3fa2cd81a4b214a5986dbb2474d2eefa268
MarshMenno <Menno.van.zoelen@gmail.com>
2024-06-03 20:24:55 +0200

    major change to redirect.js
    and minor change to chrome.js

--- assets/js/chrome.js
+++ assets/js/chrome.js
@@ -2,7 +2,7 @@
 // @ stupid tweaks for chrome since my website is build with firefox in mind ;(
 
 document.addEventListener('DOMContentLoaded', function() {
-    var userAgent = navigator.userAgent;
+    let userAgent = navigator.userAgent;
     if (userAgent.includes('Chrome')) {
         document.getElementById('chrome').style.marginBottom = '8px';
     }
--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,27 +1,32 @@
 
-function redirect(place) {
-    switch (place) {
-        case 'home': window.location.href = 'https://aRandomMenno.github.io'; break;
-        case 'projects': window.location.href = 'https://aRandomMenno.github.io/projects'; break;
-        case 'privacy': window.location.href = 'https://aRandomMenno.github.io/privacy'; break;
-        case 'notes': window.location.href = 'https://aRandomMenno.github.io/notes'; break;
-        case 'blog': window.location.href = 'https://aRandomMenno.github.io/blog'; break;
-        case 'secret': window.location.href = 'https://aRandomMenno.github.io/projects/secrets'; break;
-        case 'yt': window.open('https://youtube.com/@aRandomMenno', '_blank'); break;
-        case 'github': window.open('https://github.com/arandomMenno/', '_blank'); break;
-        case 'discord': window.open('https://discord.gg/vRpeAcKruF', '_blank'); break;
-        case 'Spijkenisse': window.open('https://en.wikipedia.org/wiki/Spijkenisse', '_blank'); break;
-        case 'Finland': window.open('https://en.wikipedia.org/wiki/Finland', '_blank'); break;
-        case 'sourceWebsite': window.open('https://github.com/aRandomMenno/aRandomMenno.github.io', '_blank');
-        case 'sourceDiscordBot': window.open('https://github.com/aRandomMenno/MarshMellow-Bot', '_blank');
-        case 'sourceTappsBox': window.open('https://github.com/aRandomMenno/TappsBox', '_blank');
-        case 'RiMusic': window.open('https://github.com/fast4x/RiMusic', '_blank'); break;
-        case 'ViMusic': window.open('https://github.com/vfsfitvnm/ViMusic', '_blank'); break;
-        case 'elsaGate': window.open('https://en.wikipedia.org/wiki/Elsagate', '_blank'); break;
-        case 'murderdrones': window.open('https://www.youtube.com/playlist?list=PLHovnlOusNLiJz3sm0d5i2Evwa2LDLdrg', '_blank'); break;
+const BASE_URL = 'https://aRandomMenno.github.io';
+const LINKS = {
+    home: `${BASE_URL}`,
+    projects: `${BASE_URL}/projects`,
+    privacy: `${BASE_URL}/privacy`,
+    notes: `${BASE_URL}/notes`,
+    blog: `${BASE_URL}/blog`,
+    secret: `${BASE_URL}/projects/secrets`,
+    yt: 'https://youtube.com/@aRandomMenno',
+    github: 'https://github.com/arandomMenno/',
+    discord: 'https://discord.gg/vRpeAcKruF',
+    Spijkenisse: 'https://en.wikipedia.org/wiki/Spijkenisse',
+    Finland: 'https://en.wikipedia.org/wiki/Finland',
+    sourceWebsite: 'https://github.com/aRandomMenno/aRandomMenno.github.io',
+    sourceDiscordBot: 'https://github.com/aRandomMenno/MarshMellow-Bot',
+    sourceTappsBox: 'https://github.com/aRandomMenno/TappsBox',
+    RiMusic: 'https://github.com/fast4x/RiMusic',
+    ViMusic: 'https://github.com/vfsfitvnm/ViMusic',
+    elsaGate: 'https://en.wikipedia.org/wiki/Elsagate',
+    murderdrones: 'https://www.youtube.com/playlist?list=PLHovnlOusNLiJz3sm0d5i2Evwa2LDLdrg'
+};
 
-        case '': window.open('', '_blank'); // @ empty entry for copy
-        default: console.log('Invalid place'); break;
+function redirect(place) {
+    const url = LINKS[place] || '';
+    if (url.startsWith(BASE_URL)) {
+        window.location.href = url;
+    } else {
+        window.open(url, '_blank');
     }
 }
 
@@ -29,4 +34,4 @@ function blogDirect(y, m, d) {
     window.location.href = `https://aRandomMenno.github.io/blog/${y}/${m}/${d}`;
 }
 
-console.log('Loaded redirect.js')
+console.info('Loaded redirect.js');
