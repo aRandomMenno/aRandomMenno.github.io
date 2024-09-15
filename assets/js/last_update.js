f98e20d5b26e7edf2398bab09afd6549ec60f7d9
aRandomMenno <109163471+aRandomMenno@users.noreply.github.com>
2024-09-15 12:16:18 +0200

    minor change

--- assets/js/last_update.js
+++ assets/js/last_update.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const info = document.getElementById('last_update')
-    const lastupdate = document.lastModified
+    let info = document.getElementById('last_update')
+    let lastupdate = document.lastModified
     info.innerHTML = lastupdate
 })
