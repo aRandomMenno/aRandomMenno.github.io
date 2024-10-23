c0089cb08a4bbf91be364a227069297a53df1d25
aRandomMenno <Menno.van.zoelen@gmail.com>
2024-10-23 16:37:29 +0200

    These two survived?

--- assets/js/last_update.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let info = document.getElementById('last_update')
-    let lastupdate = document.lastModified
-    info.innerHTML = lastupdate
-})
