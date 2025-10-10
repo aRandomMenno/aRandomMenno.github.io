24f384cc22cfe10aa225f93c264663912bf9c6c3
justMenno <JustMenno@proton.me>
2025-10-10 14:58:18 +0200

    Update JavaScript

--- js/edit.js
+++ js/edit.js
@@ -1,13 +1,8 @@
 
-function timeSinceDate() {
-  const ModifiedEl = document.getElementById("LastModified");
-  const LastModified = new Date(document.lastModified);
-  let FormattedDate = `${String(LastModified.getDate()).padStart(2, "0")}`;
-  FormattedDate += `-${String(LastModified.getMonth() + 1).padStart(2, "0")}`;
-  FormattedDate += `-${LastModified.getFullYear()} @ `;
-  FormattedDate += `${String(LastModified.getHours()).padStart(2, "0")}:`;
-  FormattedDate += `${String(LastModified.getMinutes()).padStart(2, "0")}`;
-  ModifiedEl.innerText = FormattedDate;
+const timeSinceDate = () => {
+  const modDate = new Date(document.lastModified);
+  const pad = n => String(n).padStart(2, "0");
+  document.getElementById("LastModified").innerText = `${pad(modDate.getDate())}-${pad(modDate.getMonth() + 1)}-${modDate.getFullYear()} @ ${pad(modDate.getHours())}:${pad(modDate.getMinutes())}`;
 }
 
 document.addEventListener("DOMContentLoaded", timeSinceDate);
--- js/greet.js
+++ js/greet.js
@@ -1,10 +1,4 @@
-// messages = [
-//     "Ping pong, my name is <n>Menno</n>!",
-//     "Oh hey you"re here? My name is <n>Menno</n>!",
-//     "Hello visitor, my name is <n>Menno</n>!",
-//     "Peek-a-boo! My name is <n>Menno</n>!"
-// ];
-messages = [
+const messages = [
   "Greetings, internet traveler! My name is <n>Menno</n>.",
   "Hello there! My name is <n>Menno</n>.",
   "Hey! My name is <n>Menno</n>.",
@@ -14,7 +8,7 @@ messages = [
   "Hello! My name is <n>Menno</n>.",
 ];
 
-function bday() {
+const bday = () => {
   const birthDate = new Date("2007-11-07");
   const ageDiffMs = Date.now() - birthDate.getTime();
   const ageDate = new Date(ageDiffMs);
