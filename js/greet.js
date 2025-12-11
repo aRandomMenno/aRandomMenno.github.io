160662a738bc25d04e03c3f7e7d79d29385d032b
JustMenno <JustMenno@proton.me>
2025-12-11 23:13:18 +0100

    Improve greet.js with new messages based on time of day.
    + Switch js from id's to classes. with querySelector

--- js/greet.js
+++ js/greet.js
@@ -1,12 +1,33 @@
-const messages = [
-  "Greetings, internet traveler! My name is <n>Menno</n>.",
-  "Hello there! My name is <n>Menno</n>.",
-  "Hey! My name is <n>Menno</n>.",
-  "Hi! My name is <n>Menno</n>.",
-  "Welcome! My name is <n>Menno</n>.",
-  "Hey there! My name is <n>Menno</n>.",
-  "Hello! My name is <n>Menno</n>.",
-];
+const messages = {
+  "morning": [
+    "Good morning!",
+    "Rise and shine!",
+    "Top of the morning to you!",
+    "Hello there, early bird!",
+    "Wakey wakey, eggs and bakey!",
+  ],
+  "afternoon": [
+    "Hi, internet traveler!",
+    "Good afternoon!",
+    "Hey there!",
+    "Greetings!",
+    "Hope you're having a great day!",
+  ],
+  "evening": [
+    "Evening!",
+    "Good evening!",
+    "Hope you're having a nice evening.",
+    "Greetings!",
+    "Aye, evening traveler!",
+  ],
+  "night": [
+    "Burning the midnight oil?",
+    "Good evening, night owl!",
+    "Ello there, late night visitor!",
+    "Greetings, internet traveler!",
+    "You've made it to the night shift!",
+  ]
+}
 
 function bday() {
   const birthDate = new Date("2007-11-07");
@@ -15,7 +36,13 @@ function bday() {
   return Math.abs(ageDate.getUTCFullYear() - 1970);
 }
 
+const hour = new Date().getHours();
+const timeOfDay = hour >= 6 && hour < 12 ? "morning" : hour >= 12 && hour < 18 ? "afternoon" : hour >= 18 ? "evening" : "night";
+
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("greetings").innerHTML = messages[Math.floor(Math.random() * messages.length)];
+  document.querySelector(".greetings").innerHTML = (() => {
+    const msgs = messages[timeOfDay];
+    return msgs[Math.floor(Math.random() * msgs.length)] + " My name is <n>Menno</n>.";
+  })();
   document.querySelectorAll(".age").forEach(ageEl => { ageEl.textContent = bday() });
 });
--- js/tldr.js
+++ js/tldr.js
@@ -1,7 +1,7 @@
-const tldrEl = document.getElementById("tldr");
-const tldrBtn = document.getElementById("tldr-btn");
+const tldrEl = document.querySelector(".tldr");
+const tldrBtn = document.querySelector(".tldr-btn");
 
-document.getElementById("tldr-btn").addEventListener("click", () => {
+document.querySelector(".tldr-btn").addEventListener("click", () => {
   tldrEl.style.display = tldrEl.style.display === "none" ? "block" : "none";
   tldrBtn.innerText = tldrBtn.innerText === "Show TL:DR" ? "Hide TL:DR" : "Show TL:DR";
 });
