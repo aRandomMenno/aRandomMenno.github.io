20df0265820256a224ada050e1c0f068923f3c9f
JustMenno <JustMenno@proton.me>
2025-12-12 20:30:28 +0100

    Update Index.html with new text.
    Also update a styles. Add more icons.

--- js/edit.js
+++ js/edit.js
@@ -2,7 +2,7 @@
 function timeSinceDate() {
   const modDate = new Date(document.lastModified);
   const pad = n => String(n).padStart(2, "0");
-  document.querySelector(".LastModified").innerText = `${pad(modDate.getDate())}-${pad(modDate.getMonth() + 1)}-${modDate.getFullYear()} @ ${pad(modDate.getHours())}:${pad(modDate.getMinutes())}`;
+  document.querySelector(".LastModified").innerText = `Last edit: ${pad(modDate.getDate())}-${pad(modDate.getMonth() + 1)}-${modDate.getFullYear()} @ ${pad(modDate.getHours())}:${pad(modDate.getMinutes())}`;
 }
 
 document.addEventListener("DOMContentLoaded", timeSinceDate());
