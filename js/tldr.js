9df068f034d95f9c1a133f71c7cd22da17235ebe
JustMenno <JustMenno@proton.me>
2025-12-16 21:25:45 +0100

    Fix tldr.js for multilanguage.

--- js/tldr.js
+++ js/tldr.js
@@ -3,5 +3,6 @@ const tldrBtn = document.querySelector(".tldr-btn");
 
 document.querySelector(".tldr-btn").addEventListener("click", () => {
   tldrEl.style.display = tldrEl.style.display === "none" ? "block" : "none";
-  tldrBtn.innerText = tldrBtn.innerText === "Show TL:DR" ? "Hide TL:DR" : "Show TL:DR";
+  if (currentLang === "en") tldrBtn.innerText = tldrBtn.innerText === "Show TL:DR" ? "Hide TL:DR" : "Show TL:DR";
+  if (currentLang === "nl") tldrBtn.innerText = tldrBtn.innerText === "Laat TL:DR zien" ? "Verberg TL:DR" : "Laat TL:DR zien";
 });
