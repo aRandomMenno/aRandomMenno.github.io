792bca74ac2a28f5c9dc42626488099f11f11e15
JustMenno <JustMenno@proton.me>
2025-12-16 21:03:23 +0100

    Now also the greet.js is multilang.

--- js/greet.js
+++ js/greet.js
@@ -1,32 +1,64 @@
 const messages = {
-  "morning": [
-    "Good morning!",
-    "Rise and shine!",
-    "Top of the morning to you!",
-    "Hello there, early bird!",
-    "Wakey wakey, eggs and bakey!",
-  ],
-  "afternoon": [
-    "Hi, internet traveler!",
-    "Good afternoon!",
-    "Hey there!",
-    "Greetings!",
-    "Hope you're having a great day!",
-  ],
-  "evening": [
-    "Evening!",
-    "Good evening!",
-    "Hope you're having a nice evening.",
-    "Greetings!",
-    "Aye, evening traveler!",
-  ],
-  "night": [
-    "Burning the midnight oil?",
-    "Good evening, night owl!",
-    "Ello there, late night visitor!",
-    "Greetings, internet traveler!",
-    "You've made it to the night shift!",
-  ]
+  "en": {
+    "morning": [
+      "Good morning!",
+      "Rise and shine!",
+      "Top of the morning to you!",
+      "Hello there, early bird!",
+      "Wakey wakey, eggs and bakey!",
+    ],
+    "afternoon": [
+      "Hi, internet traveler!",
+      "Good afternoon!",
+      "Hey there!",
+      "Greetings!",
+      "Hope you're having a great day!",
+    ],
+    "evening": [
+      "Evening!",
+      "Good evening!",
+      "Hope you're having a nice evening.",
+      "Greetings!",
+      "Aye, evening traveler!",
+    ],
+    "night": [
+      "Burning the midnight oil?",
+      "Good evening, night owl!",
+      "Ello there, late night visitor!",
+      "Greetings, internet traveler!",
+      "You've made it to the night shift!",
+    ]
+  },
+  "nl": {
+    "morning": [
+      "Goedemorgen!",
+      "Ben je al wakker?",
+      "Een hele goede morgen!",
+      "Hallo daar, vroege vogel!",
+      "Wakker worden, tijd voor ontbijt!",
+    ],
+    "afternoon": [
+      "Hoi, internetreiziger!",
+      "Goedemiddag!",
+      "Hallo daar!",
+      "Groeten!",
+      "Ik hoop dat je een fijne dag hebt!",
+    ],
+    "evening": [
+      "Goedenavond!",
+      "Fijne avond!",
+      "Ik hoop dat je een fijne avond hebt.",
+      "Groeten!",
+      "Ahoy, avondreiziger!",
+    ],
+    "night": [
+      "Nog laat aan het werk?",
+      "Goedenavond, nachtuil!",
+      "Hallo daar, late bezoeker!",
+      "Groeten, internetreiziger!",
+      "Je hebt de nachtdienst bereikt!",
+    ]
+  }
 }
 
 function bday() {
@@ -40,9 +72,12 @@ const hour = new Date().getHours();
 const timeOfDay = hour >= 6 && hour < 12 ? "morning" : hour >= 12 && hour < 18 ? "afternoon" : hour >= 18 ? "evening" : "night";
 
 document.addEventListener("DOMContentLoaded", () => {
+  let naam;
   document.querySelector(".greetings").innerHTML = (() => {
-    const msgs = messages[timeOfDay];
-    return msgs[Math.floor(Math.random() * msgs.length)] + " My name is <n>Menno</n>.";
+    const msgs = messages[currentLang][timeOfDay];
+    if (currentLang === "en") naam = " Mijn naam is <n>Menno</n>.";
+    if (currentLang === "nl") naam = " My name is <n>Menno</n>.";
+    return msgs[Math.floor(Math.random() * msgs.length)] + naam;
   })();
   document.querySelectorAll(".age").forEach(ageEl => { ageEl.textContent = bday() });
 });
