edda625b45b6636c7b1bb591ff89dfb16a124a7b
JustMenno <JustMenno@proton.me>
2025-12-16 20:58:24 +0100

    Update edit.js to be multilang.

--- js/edit.js
+++ js/edit.js
@@ -2,7 +2,10 @@
 function timeSinceDate() {
   const modDate = new Date(document.lastModified);
   const pad = n => String(n).padStart(2, "0");
-  document.querySelector(".LastModified").innerText = `Last edit: ${pad(modDate.getDate())}-${pad(modDate.getMonth() + 1)}-${modDate.getFullYear()} @ ${pad(modDate.getHours())}:${pad(modDate.getMinutes())}`;
+  let prefix;
+  if (currentLang === "en") prefix = "Last edit:";
+  if (currentLang === "nl") prefix = "Laatste verandering: ";
+  document.querySelector(".LastModified").innerText = `${prefix} ${pad(modDate.getDate())}-${pad(modDate.getMonth() + 1)}-${modDate.getFullYear()} @ ${pad(modDate.getHours())}:${pad(modDate.getMinutes())}`;
 }
 
 document.addEventListener("DOMContentLoaded", timeSinceDate());
