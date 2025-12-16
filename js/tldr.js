3e347fab4e253f266ca91c6e1bcf2c5e47a41bfc
JustMenno <JustMenno@proton.me>
2025-12-16 21:27:33 +0100

    TLDR -> samenvatting in het Nederlands

--- js/tldr.js
+++ js/tldr.js
@@ -4,5 +4,5 @@ const tldrBtn = document.querySelector(".tldr-btn");
 document.querySelector(".tldr-btn").addEventListener("click", () => {
   tldrEl.style.display = tldrEl.style.display === "none" ? "block" : "none";
   if (currentLang === "en") tldrBtn.innerText = tldrBtn.innerText === "Show TL:DR" ? "Hide TL:DR" : "Show TL:DR";
-  if (currentLang === "nl") tldrBtn.innerText = tldrBtn.innerText === "Laat TL:DR zien" ? "Verberg TL:DR" : "Laat TL:DR zien";
+  if (currentLang === "nl") tldrBtn.innerText = tldrBtn.innerText === "Laat samenvatting zien" ? "Verberg samenvatting" : "Laat samenvatting zien";
 });
