1e920d4106db57e2d5c061d708746464db64e804
JustMenno <JustMenno@proton.me>
2025-12-21 00:34:42 +0100

    TL:DR -> summary

--- js/tldr.js
+++ js/tldr.js
@@ -3,6 +3,6 @@ const tldrBtn = document.querySelector(".tldr-btn");
 
 document.querySelector(".tldr-btn").addEventListener("click", () => {
   tldrEl.style.display = tldrEl.style.display === "none" ? "block" : "none";
-  if (currentLang === "en") tldrBtn.innerText = tldrBtn.innerText === "Show TL:DR" ? "Hide TL:DR" : "Show TL:DR";
+  if (currentLang === "en") tldrBtn.innerText = tldrBtn.innerText === "Show summary" ? "Hide summary" : "Show summary";
   if (currentLang === "nl") tldrBtn.innerText = tldrBtn.innerText === "Laat samenvatting zien" ? "Verberg samenvatting" : "Laat samenvatting zien";
 });
